aa531402b47e76d3ff479acd4c2700e23715e068
HARTK850 <y15761576@gmail.com>
2025-06-29 16:26:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -440,7 +440,7 @@ async function generateAnswer(question) {
 }
 
 async function callGeminiAPI(prompt) {
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
